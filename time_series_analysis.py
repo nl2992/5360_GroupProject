205c96fe6d1aae36a0374eb8cd07ed73285468e1
BohanHou <houbohan0303@gmail.com>
2026-04-17 18:33:53 -0400

    variance test and push-response

--- time_series_analysis.py
+++ time_series_analysis.py
@@ -1,14 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-TYdata = pd.read_csv("TY-5minHLV.csv")
-TYdata["numTime"] = pd.to_datetime(TYdata["Date"].astype(str)+" "+TYdata["Time"].astype(str))
-TYdata = TYdata.reset_index(drop=True)
-print(TYdata.tail())
 
-p = TYdata["Close"].dropna()
-p =p.shift(-1)
-print(p)
 
 def push_response_test(price,tau=10,n_bins=50):
     """
@@ -53,29 +46,3 @@ def variance_test(price,q):
     return vr
 
 
-if __name__ == "__main__":
-    tau_list = [1, 2, 3, 4, 6, 8, 12, 16, 24, 32, 48,
-
-                72, 96, 144, 192, 288, 432, 576,
-
-                864, 1152, 1440, 2016, 2880]
-
-    for i in tau_list:
-        push_response = push_response_test(price=TYdata,tau=i)
-        plt.figure(figsize=(7,5))
-        plt.plot(push_response["push_mean"],push_response["response_mean"])
-        plt.axhline(0,linewidth=1)
-        plt.axvline(0,linewidth=1)
-        plt.xlabel("push")
-        plt.ylabel("average response")
-        plt.title(f"tau = {i}")
-        plt.tight_layout()
-        plt.show()
-
-    q_list = np.arange(1,300,1)
-    vr_list = []
-
-    for q in q_list:
-        vr=variance_test(TYdata,q)
-        vr_list.append(vr)
-        print(f"q={q:>3}, VR = {vr:.4f}")
